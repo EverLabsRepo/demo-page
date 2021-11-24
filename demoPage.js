1f511575408be91781cca653abc4fbf4f8ee78cf
Iana Kozlova <jasenkozlova@gmail.com>
2021-11-24 11:26:18 +0200

    change logic, fix mobile

--- demoPage.js
+++ demoPage.js
@@ -1,67 +1,57 @@
 //Data point value elements
 const spotsFoundDisplay = document.getElementById('dataPointValueSpotsFound');
-const adsPerPageDisplay = document.getElementById('dataPointValueAdsPerPage');
-const adRequestsPerPageDisplay = document.getElementById('dataPointValueAdRequestsPerPage');
 const networkSpeedDisplay = document.getElementById('dataPointValueNetworkSpeed');
 const scrollSpeedDisplay = document.getElementById('dataPointValueScrollSpeed');
 const floater = document.getElementById('floater');
 const runType = document.getElementById('runTypeBar');
-const floaterScript = document.getElementById('floaterScript');
-const fastScroller = document.getElementById('fastScroller');
-const slowScroller = document.getElementById('slowScroller');
-const slowInternet = document.getElementById('slowInternet');
-const hidden = document.getElementById('hidden');
-// floaterScript.style.display = 'none';
+const NO_FLOATER = 'https://cdn.avantisvideo.com/avm/js/video-loader.js?id=4102c20e-d414-456e-897a-9a26f6d87257&tagId=98';
+const FLOATER = 'https://cdn.avantisvideo.com/avm/js/video-loader.js?id=4102c20e-d414-456e-897a-9a26f6d87257&tagId=99';
+
+const types = {
+  0: document.getElementById('fastScroller'),
+  1: document.getElementById('slowScroller'),
+  2: document.getElementById('slowInternet'),
+  3: document.getElementById('hidden')
+}
+const pointsValues = {
+  0: {spotsFound: 0, networkSpeed: 'Fast', networkSpeedColor: 'green'},
+  1: {spotsFound: 0, networkSpeed: 'Fast', networkSpeedColor: 'green'},
+  2: {spotsFound: 0, networkSpeed: 'Slow', networkSpeedColor: 'red'},
+  3: {spotsFound: 0, networkSpeed: 'Fast', networkSpeedColor: 'green'},
+}
+
 
 function changeVideoType() {
   window.scroll(0, 0);
   const typeIndex = runType.selectedIndex;
+  localStorage.setItem('typeIndex', typeIndex);
   const video = document.getElementById('vid-container0') || '';
+  types[typeIndex].append(video);
+  setDataPointsValues(pointsValues[typeIndex].spotsFound, pointsValues[typeIndex].networkSpeed, pointsValues[typeIndex].networkSpeedColor);
+}
 
-  switch (typeIndex) {
-    case 0: //Fast Scroller
-      if (floater.checked) {
-        floaterScript.children[0].setAttribute('id','avantisJS');
-      }
-      else {
-        fastScroller.append(video);
-      }
-      setDataPointsValues(0, 'Fast', 'green');
-      break;
-
-    case 1: //Slow Scroller
-      if (floater.checked) {
-        floaterScript.children[0].setAttribute('id','avantisJS');
-      }
-      else {
-        slowScroller.append(video);
-      }
-      setDataPointsValues(0, 'Fast', 'green');
-      break;
-
-    case 2: //Slow Internet
-      if (floater.checked) {
-        floaterScript.children[0].setAttribute('id','avantisJS');
-      }
-      else {
-        slowInternet.append(video);
-      }
-      setDataPointsValues(0, 'Slow', 'red');
-      break;
-
-    case 3: //Non-Human
-      if (floater.checked) {
-        floaterScript.children[0].setAttribute('id','avantisJS');
-      }
-      else {
-        hidden.append(video);
-      }
-      setDataPointsValues(0, 'Fast', 'green');
-      break;
-
-    default:
-      break;
+const onLoadPage = () => {
+  let type = +localStorage.getItem('typeIndex');
+  if (!JSON.parse(localStorage.getItem('floater'))) {
+    createScript(types[type], FLOATER);
+  } else {
+    createScript(types[type], NO_FLOATER);
   }
+  floater.checked = JSON.parse(localStorage.getItem('floater'));
+  runType.selectedIndex = type;
+  setDataPointsValues(pointsValues[type].spotsFound, pointsValues[type].networkSpeed, pointsValues[type].networkSpeedColor);
+}
+
+const onClickFloater = () => {
+  localStorage.setItem('floater', !JSON.parse(localStorage.getItem('floater')));
+  window.location.reload(true);
+}
+
+const createScript = (parent, url) => {
+  const jQueryScript = document.createElement('script');
+  jQueryScript.setAttribute('src', url);
+  jQueryScript.setAttribute('id', 'avantisJS');
+  parent.appendChild(jQueryScript);
 }
 
 function setDataPointsValues(spotsFound, networkSpeed, networkSpeedColor) {
@@ -96,15 +86,3 @@ window.onscroll = function () {
   scrollSpeedDisplay.textContent = checkScrollSpeed();
   setTimeout(function () { scrollSpeedDisplay.textContent = 0; }, 800);
 };
-
-// wait for ad to appear in viewport
-var observer = new IntersectionObserver(function (entries) {
-  if (entries[0].isIntersecting === true) {
-    if (!entries[0].target.attributes.observed && !!entries[0].target.attributes.adindex) {
-      var tempEntry = entries[0].target;
-      tempEntry.setAttribute('observed', '');
-      adsPerPageDisplay.textContent = parseInt(adsPerPageDisplay.textContent) + 1;
-      adRequestsPerPageDisplay.textContent = adsPerPageDisplay.textContent;
-    }
-  }
-}, { threshold: [0] });
